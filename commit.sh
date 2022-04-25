1575c44096f8c3f7354b4809abb74c2392fc00ba
jaumeortola <jaumeortola@gmail.com>
2022-04-25 12:06:08 +0200

    add superlatives

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 git commit -a -m "$1"
 git pull --rebase
-git push origin master
+git push origin main
 cp results/lt/diccionari.txt results/lt/diccionari.old
-cd morfologik-lt
-./extrau-novetats.sh
+#cd morfologik-lt
+#./extrau-novetats.sh
