48f6f574452d0c135349c5e6bacfc6658004f622
jaumeortola <jaumeortola@gmail.com>
2023-10-03 20:03:02 +0200

    build binary spelling dicts

--- build-spelling-dicts.sh
+++ build-spelling-dicts.sh
@@ -7,16 +7,23 @@
 ltDir="../languagetool"
 ltVer="6.3-SNAPSHOT"
 
-#variants="BR PT AO MZ"
-variants="BR"
+#cd ${ltDir}
+#mvn clean package -DskipTests
+#cd -
+cd ${ltDir}/languagetool-dev
+mvn clean compile assembly:single
+cd -
+
+variants="BR PT AO MZ"
+#variants="BR"
 lang="pt"
 
-rm -rf spelling-dict/tmp
+#rm -rf spelling-dict/tmp
 mkdir spelling-dict/tmp
 
 for variant in $variants
 do
-	echo ${lang}_${variant}
+	echo "Unmunching ${lang}_${variant}..."
 	# create all forms from Hunspell dictionaries
 	unmunch spelling-dict/hunspell/${lang}_${variant}.dic spelling-dict/hunspell/${lang}_${variant}.aff | sort -u > spelling-dict/tmp/${lang}_${variant}1.txt
 	# convert to UTF-8
@@ -26,7 +33,20 @@ do
 		mv spelling-dict/tmp/${lang}_${variant}2.txt spelling-dict/tmp/${lang}_${variant}1.txt
 	fi
 	# tokenize all forms with LT tokenizer
+	echo "Tokenizing ${lang}_${variant}..."
 	cat spelling-dict/tmp/${lang}_${variant}1.txt | java -cp ${ltDir}/languagetool-standalone/target/LanguageTool-${ltVer}/LanguageTool-${ltVer}/languagetool.jar:$ltDir/languagetool-dev/target/languagetool-dev-${ltVer}-jar-with-dependencies.jar org.languagetool.dev.archive.WordTokenizer $lang | sort -u > spelling-dict/tmp/${lang}_${variant}2.txt
 	# differences after tokenization
-	diff spelling-dict/tmp/${lang}_${variant}1.txt spelling-dict/tmp/${lang}_${variant}2.txt > spelling-dict/tmp/${lang}_${variant}.diff
+	#diff spelling-dict/tmp/${lang}_${variant}1.txt spelling-dict/tmp/${lang}_${variant}2.txt > spelling-dict/tmp/${lang}_${variant}.diff
+
+	#TODO: merge spelling.txt, prohibit.txt, added.txt, removed.txt... ?
+
+	# build binary
+	freqDict="pt_PT_wordlist.xml"
+	if [ "$variant" = "BR" ]
+	then
+		freqDict=pt_BR_wordlist.xml
+	fi
+	outputDir="results/java-lt/src/main/resources/org/languagetool/resource/pt/hunspell"
+	java -cp ${ltDir}/languagetool-standalone/target/LanguageTool-${ltVer}/LanguageTool-${ltVer}/languagetool.jar org.languagetool.tools.SpellDictionaryBuilder -i spelling-dict/tmp/${lang}_${variant}2.txt -info spelling-dict/${lang}_${variant}.info -freq spelling-dict/${freqDict} -o ${outputDir}/${lang}_${variant}.dict
+	cp spelling-dict/${lang}_${variant}.info ${outputDir}
 done
