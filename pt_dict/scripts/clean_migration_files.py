fc455ba8894198ae101bf1442ac7afde106a2017
p-goulart <pedro.goulart@languagetool.org>
2023-11-16 18:53:57 +0100

    Migrate ador.txt

--- pt_dict/scripts/clean_migration_files.py
+++ pt_dict/scripts/clean_migration_files.py
@@ -1,47 +1,98 @@
+import re
 from os import path
+from typing import Callable
 
 from pt_dict.constants import TO_ADD_DIR, LATIN_1_ENCODING
 from pt_dict.dicts.dictionary import Dictionary
 from pt_dict.dicts.hunspell import HunspellDict
-from pt_dict.variants.variant import PT_BR, PT_PT_90
+from pt_dict.variants.variant import PT_BR, PT_PT_90, Variant
 
 
-def print_tagged_words(words: set[str], tags: str):
-    for word in words:
-        print(f"{word}/{tags}")
+def tag_words(words: set[str], tags: str) -> set[str]:
+    return set([f"{word}/{tags}" for word in words])
 
 
-def clean_able():
-    filename = "able.txt"
-    print(filename)
-    filepath = path.join(TO_ADD_DIR, filename)
-    pt_tags = "pms"
-    br_tags = "KXIB"
-    new_pt = set()
-    new_br = set()
+def new_lookup(br_tags: str, pt_tags: str) -> dict:
+    return {
+        'pt-BR': {
+            'tags': br_tags,
+            'new_words': set()
+        },
+        'pt-PT': {
+            'tags': pt_tags,
+            'new_words': set()
+        }
+    }
+
+
+def collect_words_from_file(filepath: str, normaliser: Callable) -> set[str]:
+    words = set()
     with open(filepath, 'r') as file:
         lines = file.read().split("\n")
         for line in lines:
             if line.startswith("#") or line == '':
                 continue
-            word = line.strip()
-            if word.endswith("is"):
-                word = word[:-2] + 'l'
-            if word not in BR_LEMMATA:
-                new_br.add(word)
-            if word not in PT_LEMMATA:
-                new_pt.add(word)
-    print(f"BR: {len(new_br)}")
-    print_tagged_words(new_br, br_tags)
-    print(f"PT: {len(new_pt)}")
-    print_tagged_words(new_pt, pt_tags)
+            words.add(normaliser(line.strip()))
+    return words
+
+
+def add_word(word: str, variant: Variant, lookup: dict):
+    if word not in LEMMATA[variant.hyphenated]:
+        lookup[variant.hyphenated]['new_words'].add(word)
+
+
+def process_file(filename: str, br_tags: str, pt_tags: str, normaliser: Callable):
+    print(filename)
+    filepath = path.join(TO_ADD_DIR, filename)
+    lookup = new_lookup(br_tags, pt_tags)
+    words = collect_words_from_file(filepath, normaliser)
+    for variant in VARIANTS:
+        for word in words:
+            add_word(word, variant, lookup)
+
+        new_words = lookup[variant.hyphenated]['new_words']
+        tags = lookup[variant.hyphenated]['tags']
+        print(f"{variant.country}: {len(new_words)}")
+        tagged = tag_words(new_words, tags)
+        print("\n".join(tagged))
+        if not DRY_RUN:
+            add_to_dic(tagged, variant)
+
+
+def add_to_dic(words: set[str], variant: Variant):
+    lines = []
+    with open(variant.dic(), 'r', encoding=LATIN_1_ENCODING) as dic_file:
+        lines.extend(dic_file.read().split("\n")[1:])
+    lines.extend(words)  # preserve order
+    with open(variant.dic(), 'w', encoding=LATIN_1_ENCODING) as dic_file:
+        dic_file.write(str(len(lines)) + "\n")
+        dic_file.write("\n".join(lines))
+
+
+def clean_able():
+    def normaliser(w: str) -> str:
+        if w.endswith("is"):
+            return w[:-2] + 'l'
+        return w
+    process_file('able.txt', 'KXIB', 'pms', normaliser)
+
+
+def clean_ador():
+    def normaliser(w: str) -> str:
+        return re.compile('([aei]dor)(es|as?)?$').sub(r"\1", w)
+    process_file('ador.txt', 'DR', 'fp', normaliser)
 
 
 if __name__ == "__main__":
+    VARIANTS = [PT_BR, PT_PT_90]
     br_dict = Dictionary()
     pt_dict = Dictionary()
     br_dict.collect_lemmata_from_file(PT_BR.dic(), HunspellDict.pattern, encoding=LATIN_1_ENCODING, offset=1)
     pt_dict.collect_lemmata_from_file(PT_PT_90.dic(), HunspellDict.pattern, encoding=LATIN_1_ENCODING, offset=1)
-    BR_LEMMATA = br_dict.lemmata
-    PT_LEMMATA = pt_dict.lemmata
+    LEMMATA = {
+        'pt-BR': br_dict.lemmata,
+        'pt-PT': pt_dict.lemmata
+    }
+    DRY_RUN = False
     clean_able()
+    clean_ador()
