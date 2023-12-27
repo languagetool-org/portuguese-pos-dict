1676982283e2e336c66c666abdd4a6bafcac9da3
p-goulart <pedro.goulart@languagetool.org>
2023-12-27 19:32:04 +0100

    Prepare for next release

---
+++ pt_dict/console_utils.py
@@ -0,0 +1,65 @@
+from os import path
+from typing import Literal
+
+from spylls.hunspell import Dictionary
+
+from pt_dict.constants import HUNSPELL_DIR
+from pt_dict.variants.variant import PT_BR, Variant, PT_PT_90, PT_PT_45
+
+
+class ConsoleUtils:
+    def __init__(self, mode: Literal["main", "compounds"] = 'main'):
+        self._mode: Literal["main", "compounds"] = mode
+        self.br = None
+        self.pt90 = None
+        self.pt45 = None
+        self.br_c = None
+        self.pt90_c = None
+        self.pt45_c = None
+        self.load_dictionaries()
+
+    @property
+    def mode(self) -> str:
+        return self._mode
+
+    @mode.setter
+    def mode(self, mode_name: Literal["main", "compounds"]) -> None:
+        do_reload = self._mode != mode_name
+        self._mode = mode_name
+        if do_reload:
+            self.reload()
+
+    def compound_mode(self):
+        self.mode = "compounds"
+
+    def normal_mode(self):
+        self.mode = "main"
+
+    def load_dictionary(self, variant: Variant) -> Dictionary:
+        if variant.country == 'BR':
+            variant_code = variant.underscored
+        else:
+            variant_code = variant.underscored_with_agreement
+        if self.mode == 'compounds':
+            dict_path = path.join(HUNSPELL_DIR, "compounds", variant_code)
+        else:
+            dict_path = path.join(HUNSPELL_DIR, variant_code)
+        return Dictionary.from_files(dict_path)
+
+    def load_dictionaries(self):
+        self.br = self.load_dictionary(PT_BR)
+        self.pt90 = self.load_dictionary(PT_PT_90)
+        self.pt45 = self.load_dictionary(PT_PT_45)
+
+    def reload(self):
+        self.load_dictionaries()
+
+    def lookup(self, word: str, reload=False) -> dict[str, bool]:
+        if reload:
+            self.reload()
+        return {
+            'br': self.br.lookup(word),
+            'pt90': self.pt90.lookup(word),
+            'pt45': self.pt45.lookup(word),
+        }
+
---
+++ pt_dict/scripts/test_words.py
@@ -0,0 +1,52 @@
+"""This script tests to see if words are recognised by the Hunspell binary, using our Hunspell source files."""
+import sys
+from os import path
+
+from spylls.hunspell import Dictionary
+
+from pt_dict.constants import HUNSPELL_DIR
+from pt_dict.variants.variant import DIC_VARIANTS, PT_PT_90, PT_PT_45, PT_BR
+
+
+def main():
+    COMPOUND = True
+    absent: dict[str, set] = dict()
+    present: dict[str, set] = dict()
+    words = set()
+    test_filepath = sys.argv[1]
+    loop_variants = [PT_BR]
+    with open(test_filepath, 'r') as test_file:
+        words.update(test_file.read().split("\n"))
+    for variant in loop_variants:
+        print(f"Checking {variant}...")
+        if variant.country == 'BR':
+            variant_code = variant.underscored
+        else:
+            variant_code = variant.underscored_with_agreement
+        absent[variant_code] = set()
+        present[variant_code] = set()
+        if COMPOUND:
+            dict_path = path.join(HUNSPELL_DIR, "compounds", variant_code)
+        else:
+            dict_path = path.join(HUNSPELL_DIR, variant_code)
+        dictionary = Dictionary.from_files(dict_path)
+        for word in words:
+            if dictionary.lookup(word):
+                present[variant_code].add(word)
+            else:
+                absent[variant_code].add(word)
+        print(f"Present: {len(present[variant_code])}")
+        print(f"Absent: {len(absent[variant_code])}")
+    present_in_all = set.intersection(*present.values())
+    absent_in_all = set.intersection(*absent.values())
+    print(f"Present in all: {len(present_in_all)}")
+    print(f"Absent in all: {len(absent_in_all)}")
+    # print("OOGA")
+    # print("\n".join(sorted(words.difference(absent_in_all))))
+    # print("FRED")
+    print("\n".join(sorted(absent_in_all)))
+    # print("\n".join(sorted(words.difference(present_in_all))))
+
+
+if __name__ == "__main__":
+    main()
